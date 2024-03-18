41e407814ffa667eda3d07c67b54cc6d03f5dfc3
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-18 10:09:41 -0500

    initial project commit

---
+++ src/main/java/com/llama/api/LlamaApiApplication.java
@@ -0,0 +1,13 @@
+package com.llama.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlamaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlamaApiApplication.class, args);
+	}
+
+}
