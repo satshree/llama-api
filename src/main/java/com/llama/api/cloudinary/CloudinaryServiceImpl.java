ec62949c37bbc7570a128a215f28ed42dfbc37cd
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-19 11:27:25 -0500

    add cloudinary service

---
+++ src/main/java/com/llama/api/cloudinary/CloudinaryServiceImpl.java
@@ -0,0 +1,28 @@
+package com.llama.api.cloudinary;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class CloudinaryServiceImpl {
+
+    @Autowired
+    Cloudinary cloudinary;
+
+    public String uploadImage(MultipartFile file, String productImageID) throws IOException {
+        // DEFINE FOLDER
+        HashMap<Object, Object> options = new HashMap<>();
+        options.put("folder", "llama/images/[" + productImageID + "]");
+
+        Map result = cloudinary.uploader().upload(file, options);
+
+        return result.get("url").toString();
+    }
+}
