98ebc418483003c942c66e66139141c9e9fac9fd
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-20 11:03:08 -0500

    remove unused imports

--- src/main/java/com/llama/api/users/repository/UserRepository.java
+++ src/main/java/com/llama/api/users/repository/UserRepository.java
@@ -2,9 +2,7 @@ package com.llama.api.users.repository;
 
 import com.llama.api.users.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
