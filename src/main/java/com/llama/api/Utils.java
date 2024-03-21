6da3fc591021bdd451aac0391379300b36aea34d
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-21 15:45:10 -0500

    update date parse util

--- src/main/java/com/llama/api/Utils.java
+++ src/main/java/com/llama/api/Utils.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class Utils {
     public static String parseDate(Date dateToParse) {
         if (dateToParse != null) {
-            return new SimpleDateFormat("dd. MMM, yyyy HH:mm a").format(dateToParse);
+            return new SimpleDateFormat("dd. MMM, yyyy hh:mm a").format(dateToParse);
         }
 
         return "";
