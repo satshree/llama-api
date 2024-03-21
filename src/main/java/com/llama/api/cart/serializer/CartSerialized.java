875068a8d69e8a195f48a7144d48dba23fccdca9
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-21 11:56:53 -0500

    add cart serializers

---
+++ src/main/java/com/llama/api/cart/serializer/CartSerialized.java
@@ -0,0 +1,31 @@
+package com.llama.api.cart.serializer;
+
+import com.llama.api.Utils;
+import com.llama.api.cart.models.Cart;
+import com.llama.api.users.serializer.SimpleUserSerialized;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartSerialized {
+    String id;
+    String updated;
+    Double total;
+    SimpleUserSerialized user;
+    List<CartItemSerialized> items;
+
+    public static CartSerialized serialize(Cart cart) {
+        return new CartSerialized(
+                cart.getId().toString(),
+                Utils.parseDate(cart.getUpdated()),
+                cart.getTotal(),
+                SimpleUserSerialized.serialize(cart.getUser().getUser()),
+                CartItemSerialized.serialize(cart.getCartItems())
+        );
+    }
+}
