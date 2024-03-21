74153ba4cc6522d7bac16037ad50a90cba087f24
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-21 17:56:04 -0500

    fetch product api

--- src/main/java/com/llama/api/products/controllers/WebsiteProductController.java
+++ src/main/java/com/llama/api/products/controllers/WebsiteProductController.java
@@ -1,24 +1,23 @@
 package com.llama.api.products.controllers;
 
+import com.llama.api.exceptions.ResourceNotFound;
 import com.llama.api.products.serializer.ProductSerialized;
 import com.llama.api.products.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/website/products")
+@RequestMapping("/api/website/product")
 public class WebsiteProductController {
     @Autowired
     ProductService productService;
 
+    @GetMapping("/")
     public ResponseEntity<List<ProductSerialized>> getProducts(@RequestParam(required = false) String name, @RequestParam(required = false) String sku) {
         List<ProductSerialized> allProducts = new ArrayList<>();
 
@@ -31,5 +30,10 @@ public class WebsiteProductController {
         }
 
         return ResponseEntity.ok(allProducts);
+    }
+
+    @GetMapping("/{id}/")
+    public ResponseEntity<ProductSerialized> getProduct(@PathVariable("id") String id) throws ResourceNotFound {
+        return ResponseEntity.ok(ProductSerialized.serialize(productService.getProduct(id)));
     }
 }
