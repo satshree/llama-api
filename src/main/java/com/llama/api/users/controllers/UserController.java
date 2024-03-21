7b19c487640b2f230e86ed6c40a523316edee0d9
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-21 15:47:49 -0500

    get user info api

---
+++ src/main/java/com/llama/api/users/controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.llama.api.users.controllers;
+
+import com.llama.api.users.serializer.UserSerialized;
+import com.llama.api.users.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600) // change later to only accept from frontend applications
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/")
+    public ResponseEntity<UserSerialized> getUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        return ResponseEntity.ok(UserSerialized.serialize(userService.getUserByUsername(username)));
+    }
+}
