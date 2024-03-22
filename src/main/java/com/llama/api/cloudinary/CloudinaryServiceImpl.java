1d47babc06055de6ff7ec9565de8b93acfcbb3b6
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-22 13:12:24 -0500

    remove unused imports

--- src/main/java/com/llama/api/cloudinary/CloudinaryServiceImpl.java
+++ src/main/java/com/llama/api/cloudinary/CloudinaryServiceImpl.java
@@ -2,12 +2,10 @@ package com.llama.api.cloudinary;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import com.llama.api.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
