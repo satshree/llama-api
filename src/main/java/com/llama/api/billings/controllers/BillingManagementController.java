5fa1a49c079fd902611500d615db1fcd24e8ac5e
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-25 12:42:09 -0500

    add management billing rest api

--- src/main/java/com/llama/api/billings/controllers/BillingManagementController.java
+++ src/main/java/com/llama/api/billings/controllers/BillingManagementController.java
@@ -1,11 +1,34 @@
 package com.llama.api.billings.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.llama.api.billings.serializer.BillingSerialized;
+import com.llama.api.billings.services.BillingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "*", maxAge = 3600) // change later to only accept from frontend applications
 @RestController
 @RequestMapping("/api/management/billing")
 public class BillingManagementController {
+    @Autowired
+    BillingService billingService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<BillingSerialized>> getBillings() {
+        return ResponseEntity.ok(billingService.getAllBillingSerialized());
+    }
+
+    @DeleteMapping("/{id}/")
+    public ResponseEntity<Map<String, String>> deleteBilling(@PathVariable("id") String id) {
+        billingService.deleteBill(id);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Bill deleted");
+
+        return ResponseEntity.ok(response);
+    }
 }
